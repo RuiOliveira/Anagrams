8667fb748719e97dcd7b1f15f3f73c381a8eaa01
RuiOliveira <rui18oliver@gmail.com>
2017-06-04 12:31:48 +0100

    Insert a new description
    Insert a new description

--- src/CheckAnagrams.java
+++ src/CheckAnagrams.java
@@ -1,17 +1,23 @@
 import java.util.Arrays;
 
 /**
- * Created by NB22440 on 31/05/2017.
+ * @author      Rui Oliveira <rui18oliver@gmail.com>
+ * @Section 1
+ * Question 2
  */
+
 public class CheckAnagrams {
 
+    /**
+     * @Description: Call CheckAnagram and print the results in log
+     */
     public static void main(String[] args) {
 
-        boolean result1 = isAnagram("Punishment","Nine Thumps");
-        boolean result2 = isAnagram("The Morse code","Here come dots");
-        boolean result3 = isAnagram("Snooze alarms","Alas! No more Zs");
-        boolean result4 = isAnagram("Halley's Comet","Shall yet come");
-        boolean result5 = isAnagram("One good turn deserves another.","Do rogues endorse that? No,never!");
+        boolean result1 = CheckAnagram("Punishment","Nine Thumps");
+        boolean result2 = CheckAnagram("The Morse code","Here come dots");
+        boolean result3 = CheckAnagram("Snooze alarms","Alas! No more Zs");
+        boolean result4 = CheckAnagram("Halley's Comet","Shall yet come");
+        boolean result5 = CheckAnagram("One good turn deserves another.","Do rogues endorse that? No,never!");
 
         System.out.println(result1);
         System.out.println(result2);
@@ -23,15 +29,17 @@ public class CheckAnagrams {
 
     }
 
-    public static boolean isAnagram(String firstWord, String secondWord) {
+    /**
+     * @Description: The obbjectve of this method is convert, clear all with spaces (I use a regular expression) from the two input
+     * strings and create a character array then I compare the two character arrays and I print return a boolean
+     * */
+    public static boolean CheckAnagram(String firstWord, String secondWord) {
 
         firstWord.toLowerCase();
         secondWord.toLowerCase();
 
         char[] word1 = firstWord.replaceAll("[\\s]", "").toCharArray();
         char[] word2 = secondWord.replaceAll("[\\s]", "").toCharArray();
-
-
 
         Arrays.sort(word1);
         Arrays.sort(word2);
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
+import java.util.Objects;
+
 /**
- * Created by NB22440 on 01/06/2017.
+ * @author      Rui Oliveira <rui18oliver@gmail.com>
+ * @Section 1
+ * Question 2
  */
+
 public class Main {
+
+    /**
+     * @Description: Call fixSpelling and print the results in log
+     */
+
     public static void main(String[] args) {
         System.out.println(fixSpelling("hostleworld"));
     }
+
+    /**
+     * @Description: In java "==" operator checks whether the references to objects are equal (memory positions).
+     * string.equals() function checks the actual contents of a string
+     */
+
     public static String fixSpelling(String name) {
+
         String wordToCheck = new String(name);
-        if(wordToCheck == "hostleworld" ) {
+
+        //if(wordToCheck == "hostleworld" ) (ERROR!)
+
+        if(Objects.equals(wordToCheck, "hostleworld")) {
             name = "hostelworld";
         }
         else {
@@ -16,3 +36,4 @@ public class Main {
         return name;
     }
 }
+
